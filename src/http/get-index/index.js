76eb226fe54d09aba7ead7b99889c54c1ed14c2e
retrohacker <retrohacker@protonmail.com>
2019-02-18 14:31:16 -0800

    spa

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -1,32 +1,13 @@
-var sortPosts = require('@architect/shared/sortPosts')
-var blog = require('@architect/shared/blog')
-var template = require('@architect/shared/template')
-var months = require('@architect/shared/months')
+let arc = require('@architect/functions')
 
-exports.handler = async function http(req) {
-  return router(req.path)
-}
-
-function router(path) {
-  return renderHome()
-  const isHome = /^\/home\/?$/
-  const isYear = /^\/date\/([0-9]{4})\/?$/
-  const isMonth = /^\/date\/([0-9]{4})\/([0-9]{2})\/?$/
-  const isFile = /^\/file\/(.*)$/
-
-  if(isHome.test(path)) {
-    return renderHome(...isHome.exec(path).slice(1));
+exports.handler = arc.proxy.public({
+  async ssr(req) {
+    let headers = {
+      'Content-Type': 'text/html',
+      'Status': '302 Redirect',
+      'Location': '/home'
+    }
+    let body = ''
+    return { headers, body }
   }
-
-  if(isYear.test(path)) {
-    return renderYear(...isYear.exec(path).slice(1));
-  }
-
-  if(isMonth.test(path)) {
-    return renderMonth(...isMonth.exec(path).slice(1));
-  }
-
-  if(isFile.test(path)) {
-    return renderFile(...isFile.exec(path).slice(1));
-  }
-}
+})
